f7595c553cd3da1f827139670898b71563e36c7d
Rick Busarow <RBusarow@users.noreply.github.com>
2021-07-22 22:36:43 -0500

    Move TangleViewModelFactory to internal package (#70)
    https://github.com/RBusarow/Tangle/pull/70

--- tangle-viewmodel-api/src/main/java/tangle/viewmodel/tangle.kt
+++ tangle-viewmodel-api/src/main/java/tangle/viewmodel/tangle.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import tangle.inject.InternalTangleApi
+import tangle.viewmodel.internal.TangleViewModelFactory
 import kotlin.LazyThreadSafetyMode.NONE
 
 /**
