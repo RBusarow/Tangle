e57a1f76530ed5d1ddcbe71c823e3a4633622222
Rick Busarow <rickbusarow@gmail.com>
2021-07-27 00:13:26 -0500

    rename the compose `tangle` function to `tangleViewModel`

--- tangle-viewmodel-compose/src/main/java/tangle/inject/compose/compose.kt
+++ tangle-viewmodel-compose/src/main/java/tangle/inject/compose/compose.kt
@@ -29,9 +29,11 @@ import tangle.viewmodel.internal.TangleViewModelFactory
  * Returns an existing [VMInject][tangle.viewmodel.VMInject]-annotated [ViewModel]
  * or creates a new one scoped to the current navigation graph present on
  * the NavController back stack.
+ *
+ * @since 0.10.0
  */
 @Composable
-public inline fun <reified VM : ViewModel> tangle(
+public inline fun <reified VM : ViewModel> tangleViewModel(
   viewModelStoreOwner: ViewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current) {
     "No ViewModelStoreOwner was provided via LocalViewModelStoreOwner"
   }
