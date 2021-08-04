0e19303cc226f582935c42e9e6aa47db92f26b68
Rick Busarow <rickbusarow@gmail.com>
2021-08-04 15:46:10 -0500

    create `release.sh`

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# Publish Maven release
+./gradlew publish --no-daemon --no-parallel
+
+# Close Maven release
+./gradlew closeAndReleaseRepository --no-daemon --no-parallel
+
+# Publish to Gradle Plugin Portal
+./gradlew publishPlugins
+
+# Create new website docs version
+./gradlew versionDocs
+
+echo
+echo ' ___ _   _  ___ ___ ___  ___ ___'
+echo '/ __| | | |/ __/ __/ _ \/ __/ __|'
+echo '\__ \ |_| | (_| (_|  __/\__ \__ \'
+echo '|___/\__,_|\___\___\___||___/___/'
+echo
+echo
+echo The release is done and a new docs version has been created for Docusaurus.
+echo
+echo These changes need to get merged into main and published.
+echo
+echo
