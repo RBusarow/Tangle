e2d788dbf21f45596610f8f8f8e55fa3e0b98d83
Rick Busarow <rickbusarow@gmail.com>
2021-09-02 12:14:05 -0500

    update kotlin-compile-testing to 1.4.4

--- dependabot-bridge/build.gradle.kts
+++ dependabot-bridge/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
 
   dependencySync("com.android.tools.build:gradle:7.0.1")
   dependencySync("com.github.javaparser:javaparser-symbol-solver-core:3.23.0")
-  dependencySync("com.github.tschuchortdev:kotlin-compile-testing:1.4.3")
+  dependencySync("com.github.tschuchortdev:kotlin-compile-testing:1.4.4")
   dependencySync("com.google.accompanist:accompanist-coil:0.15.0")
   dependencySync("com.google.android.material:material:1.4.0")
   dependencySync("com.google.auto.service:auto-service-annotations:1.0")
