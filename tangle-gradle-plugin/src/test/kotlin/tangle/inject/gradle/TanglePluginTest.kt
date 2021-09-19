0ab15263b9db5d9c25ab9b718e78697172066348
Rick Busarow <RBusarow@users.noreply.github.com>
2021-09-19 14:27:30 -0500

    Fix how Tangle plugin detects the AGP plugin (#334)
    fixes #332
    fixes #333
    
    https://github.com/RBusarow/Tangle/pull/334

---
+++ tangle-gradle-plugin/src/test/kotlin/tangle/inject/gradle/TanglePluginTest.kt
@@ -0,0 +1,385 @@
+/*
+ * Copyright (C) 2021 Rick Busarow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tangle.inject.gradle
+
+import org.junit.jupiter.api.TestFactory
+
+public class TanglePluginTest : BasePluginTest() {
+
+  @TestFactory
+  fun `default application should apply Anvil`() = test {
+
+    //language=kotlin
+    module(
+      """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      ${listDepsTasks()}
+      """.trimIndent()
+    )
+
+    // The Anvil configuration only exists if Anvil is applied, so this would fail without the plugin
+    build("deps").shouldSucceed()
+  }
+
+  @TestFactory
+  fun `default application should add Tangle dependencies`() = test {
+
+    //language=kotlin
+    module(
+      """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      ${listDepsTasks()}
+      """.trimIndent()
+    )
+
+    build("deps").deps() shouldBe listOf(
+      "anvil com.rickbusarow.tangle:tangle-fragment-compiler",
+      "anvil com.rickbusarow.tangle:tangle-viewmodel-compiler",
+      "anvil com.rickbusarow.tangle:tangle-work-compiler",
+      "implementation com.rickbusarow.tangle:tangle-api",
+      "implementation com.rickbusarow.tangle:tangle-fragment-api",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-activity",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-api",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-fragment",
+      "implementation com.rickbusarow.tangle:tangle-work-api"
+    )
+  }
+
+  @TestFactory
+  fun `disabling fragments in config should disable their dependencies`() = test {
+
+    //language=kotlin
+    module(
+      """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      tangle {
+        fragmentsEnabled = false // default is true
+      }
+
+      ${listDepsTasks()}
+      """.trimIndent()
+    )
+
+    build("deps").deps() shouldBe listOf(
+      "anvil com.rickbusarow.tangle:tangle-viewmodel-compiler",
+      "anvil com.rickbusarow.tangle:tangle-work-compiler",
+      "implementation com.rickbusarow.tangle:tangle-api",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-activity",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-api",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-fragment",
+      "implementation com.rickbusarow.tangle:tangle-work-api"
+    )
+  }
+
+  @TestFactory
+  fun `disabling work in config should disable its dependencies`() = test {
+
+    //language=kotlin
+    module(
+      """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      tangle {
+        workEnabled = false // default is true
+      }
+
+      ${listDepsTasks()}
+      """.trimIndent()
+    )
+
+    build("deps").deps() shouldBe listOf(
+      "anvil com.rickbusarow.tangle:tangle-fragment-compiler",
+      "anvil com.rickbusarow.tangle:tangle-viewmodel-compiler",
+      "implementation com.rickbusarow.tangle:tangle-api",
+      "implementation com.rickbusarow.tangle:tangle-fragment-api",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-activity",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-api",
+      "implementation com.rickbusarow.tangle:tangle-viewmodel-fragment"
+    )
+  }
+
+  @TestFactory
+  fun `disabling viewModels in config should disable its dependencies`() = test {
+
+    //language=kotlin
+    module(
+      """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      tangle {
+        viewModelOptions {
+          enabled = false // default is true
+        }
+      }
+
+      ${listDepsTasks()}
+      """.trimIndent()
+    )
+
+    build("deps").deps() shouldBe listOf(
+      "anvil com.rickbusarow.tangle:tangle-fragment-compiler",
+      "anvil com.rickbusarow.tangle:tangle-work-compiler",
+      "implementation com.rickbusarow.tangle:tangle-api",
+      "implementation com.rickbusarow.tangle:tangle-fragment-api",
+      "implementation com.rickbusarow.tangle:tangle-work-api"
+    )
+  }
+
+  @TestFactory
+  fun `disabling viewModels fragments in config should disable the viewmodel fragment api dependency`() =
+    test {
+
+      //language=kotlin
+      module(
+        """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      tangle {
+        viewModelOptions {
+          fragmentsEnabled = false // default is true
+        }
+      }
+
+      ${listDepsTasks()}
+        """.trimIndent()
+      )
+
+      build("deps").deps() shouldBe listOf(
+        "anvil com.rickbusarow.tangle:tangle-fragment-compiler",
+        "anvil com.rickbusarow.tangle:tangle-viewmodel-compiler",
+        "anvil com.rickbusarow.tangle:tangle-work-compiler",
+        "implementation com.rickbusarow.tangle:tangle-api",
+        "implementation com.rickbusarow.tangle:tangle-fragment-api",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-activity",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-api",
+        "implementation com.rickbusarow.tangle:tangle-work-api"
+      )
+    }
+
+  @TestFactory
+  fun `disabling viewModels activities in config should disable the viewmodel activity api dependency`() =
+    test {
+
+      //language=kotlin
+      module(
+        """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      tangle {
+        viewModelOptions {
+          activitiesEnabled = false // default is true
+        }
+      }
+
+      ${listDepsTasks()}
+        """.trimIndent()
+      )
+
+      build("deps").deps() shouldBe listOf(
+        "anvil com.rickbusarow.tangle:tangle-fragment-compiler",
+        "anvil com.rickbusarow.tangle:tangle-viewmodel-compiler",
+        "anvil com.rickbusarow.tangle:tangle-work-compiler",
+        "implementation com.rickbusarow.tangle:tangle-api",
+        "implementation com.rickbusarow.tangle:tangle-fragment-api",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-api",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-fragment",
+        "implementation com.rickbusarow.tangle:tangle-work-api"
+      )
+    }
+
+  @TestFactory
+  fun `enabling viewModels compose in config should disable the viewmodel compose api dependency`() =
+    test {
+
+      //language=kotlin
+      module(
+        """
+      plugins {
+        id("com.android.library")
+        kotlin("android")
+        id("com.rickbusarow.tangle")
+      }
+
+      android {
+        compileSdk = 30
+
+        defaultConfig {
+          minSdk = 23
+          targetSdk = 30
+        }
+      }
+
+      tangle {
+        viewModelOptions {
+          composeEnabled = true // default is false
+        }
+      }
+
+      ${listDepsTasks()}
+        """.trimIndent()
+      )
+
+      build("deps").deps() shouldBe listOf(
+        "anvil com.rickbusarow.tangle:tangle-fragment-compiler",
+        "anvil com.rickbusarow.tangle:tangle-viewmodel-compiler",
+        "anvil com.rickbusarow.tangle:tangle-work-compiler",
+        "implementation com.rickbusarow.tangle:tangle-api",
+        "implementation com.rickbusarow.tangle:tangle-fragment-api",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-activity",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-api",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-compose",
+        "implementation com.rickbusarow.tangle:tangle-viewmodel-fragment",
+        "implementation com.rickbusarow.tangle:tangle-work-api"
+      )
+    }
+
+  @TestFactory
+  fun `build will fail if applied to a module without AGP`() = test {
+
+    //language=kotlin
+    module(
+      """
+      plugins {
+        kotlin("jvm")
+        id("com.rickbusarow.tangle")
+      }
+
+      tangle {
+        viewModelOptions {
+          composeEnabled = true // default is false
+        }
+      }
+
+      ${listDepsTasks()}
+      """.trimIndent()
+    )
+
+    tasks("test").shouldFailWithMessage(
+      "A problem occurred configuring project ':module'.\n" +
+        "> Tangle is applied to project ':module', but no Android plugin has been applied.  " +
+        "Tangle serves no purpose unless the project is Android and the Kotlin plugin is applied."
+    )
+  }
+
+  public fun listDepsTasks() = """
+  tasks.register("deps") {
+    doLast {
+      listOf("anvil", "api", "implementation")
+        .forEach { config ->
+          project.configurations
+            .named(config)
+            .get()
+            .dependencies
+            .forEach { println("${'$'}config ${'$'}{it.group}:${'$'}{it.name}") }
+        }
+    }
+  }
+  """.trimIndent()
+}
---
+++ tangle-gradle-plugin/src/test/kotlin/tangle/inject/gradle/TestScope.kt
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2021 Rick Busarow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tangle.inject.gradle
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.TestInfo
+import java.io.File
+
+public data class TestScope(
+  val testInfo: TestInfo,
+  val gradleVersion: String,
+  val kotlinVersion: String,
+  val agpVersion: String,
+  val anvilVersion: String
+) {
+
+  public val classNameDir: File by lazy {
+    val className = testInfo.testClass
+      .get()
+      .simpleName
+      .replace("[^a-zA-Z0-9]".toRegex(), "_")
+
+    File("build/tests/$className").also {
+      it.mkdir()
+    }
+  }
+
+  public val testProjectDir: File by lazy {
+    val testName = testInfo.displayName
+      .replace("[^a-zA-Z0-9]".toRegex(), "_")
+      .replace("_{2,}".toRegex(), "_")
+      .removeSuffix("_")
+
+    val scopeName = toString()
+      .replace("[^a-zA-Z0-9]".toRegex(), "_")
+      .replace("_{2,}".toRegex(), "_")
+      .removeSuffix("_")
+
+    File(classNameDir, "$testName/$scopeName").also {
+      it.mkdir()
+    }
+  }
+
+  public fun gradleRunner(): GradleRunner = GradleRunner.create()
+    .forwardOutput()
+    .withGradleVersion(gradleVersion)
+    .withPluginClasspath()
+    // .withDebug(true)
+    .withProjectDir(testProjectDir)
+
+  public fun projectBuildFile(): File {
+    testProjectDir.mkdirs()
+    return File(testProjectDir, "build.gradle.kts").also {
+      it.writeText(
+        """buildscript {
+            |  repositories {
+            |    mavenCentral()
+            |    google()
+            |    maven("https://plugins.gradle.org/m2/")
+            |  }
+            |  dependencies {
+            |    classpath("com.squareup.anvil:gradle-plugin:$anvilVersion")
+            |    classpath("com.android.tools.build:gradle:$agpVersion")
+            |    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+            |  }
+            |}
+            |allprojects {
+            |  repositories {
+            |    mavenCentral()
+            |    google()
+            |    maven("https://plugins.gradle.org/m2/")
+            |  }
+            |}
+            |""".trimMargin()
+      )
+    }
+  }
+
+  public fun settingsFile(): File {
+    testProjectDir.mkdirs()
+    return File(testProjectDir, "settings.gradle.kts").also {
+      it.writeText(
+        """pluginManagement {
+          |  repositories {
+          |    gradlePluginPortal()
+          |    google()
+          |  }
+          |  resolutionStrategy {
+          |    eachPlugin {
+          |      if (requested.id.id.startsWith("com.android")) {
+          |        useVersion("$agpVersion")
+          |      }
+          |      if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+          |        useVersion("$kotlinVersion")
+          |      }
+          |    }
+          |  }
+          |}
+          |include(":module")
+          |""".trimMargin()
+      )
+    }
+  }
+
+  public fun module(buildFile: String) {
+    File(testProjectDir, "module/src/main").mkdirs()
+    File(testProjectDir, "module/src/main/AndroidManifest.xml")
+      .writeText("<manifest package=\"module.foo\" />")
+    File(testProjectDir, "module/build.gradle.kts")
+      .writeText(buildFile)
+  }
+
+  public fun build(vararg tasks: String): BuildResult {
+    File(testProjectDir, "gradle.properties")
+      .writeText("org.gradle.jvmargs=-Xmx3g -XX:MaxMetaspaceSize=1g")
+    settingsFile()
+    projectBuildFile()
+    return gradleRunner().withArguments(*tasks).build()
+  }
+
+  public fun tasks(vararg tasks: String): GradleRunner {
+    File(testProjectDir, "gradle.properties")
+      .writeText("org.gradle.jvmargs=-Xmx3g -XX:MaxMetaspaceSize=1g")
+    settingsFile()
+    projectBuildFile()
+    return gradleRunner().withArguments(*tasks)
+  }
+
+  override fun toString(): String {
+    return "[gradle='$gradleVersion', kotlin='$kotlinVersion', agp='$agpVersion', anvil='$anvilVersion']"
+  }
+}
